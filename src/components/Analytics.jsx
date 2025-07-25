75356bffddfe7db941bdce7fd25840a6ccf47ffe
Hakan Türkmen <hakannturkmen@gmail.com>
2025-07-25 21:38:36 +0300

    speed

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,10 +1,14 @@
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 export const VercelAnalytics = () => {
   return (
-    <Analytics 
-      mode="production"
-      debug={import.meta.env.DEV}
-    />
+    <>
+      <Analytics 
+        mode="production"
+        debug={import.meta.env.DEV}
+      />
+      <SpeedInsights />
+    </>
   )
 } 
